186e17532f6f7349dc8c2aee00b30228eaeb52af
uzinok <41639106+uzinok@users.noreply.github.com>
2026-09-17 02:23:55 +0300

    делает сетку для .example

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,7 @@
 /* в этот файл добавляет скрипты*/
+const slider = document.querySelector('.before-after');
+const range = document.querySelector('.before-after__range-js');
+
+range.addEventListener('input', () => {
+  slider.style.setProperty('--value', range.value * 0.1 + '%');
+});
