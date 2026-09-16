1cf965272038145a04b9e11e55256cab93fa254e
uzinok <uzinok@users.noreply.github.com>
2026-09-16 23:30:11 +0000

    Deploying to gh-pages from @ uzinok/198751-cat-energy-3@d3f14b1c06a54004d64583b976f4816a6caf6669 🚀

--- 7/scripts/index.js
+++ 7/scripts/index.js
@@ -1,1 +1,1 @@
-var r=document.querySelector(".before-after"),e=document.querySelector(".before-after__range-js");e.addEventListener("input",()=>{r.style.setProperty("--value",e.value*.1+"%")});
+var r=document.querySelector(".before-after"),e=document.querySelector(".before-after__range-js");e.addEventListener("input",()=>{r.style.setProperty("--value",`${e.value*.1}%`)});
