a7e8d33838a7751b7d137dde24ac4b7a5ebeeef1
uzinok <41639106+uzinok@users.noreply.github.com>
2026-09-17 02:29:39 +0300

    исправляет ошибки по линтеру

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -3,5 +3,5 @@ const slider = document.querySelector('.before-after');
 const range = document.querySelector('.before-after__range-js');
 
 range.addEventListener('input', () => {
-  slider.style.setProperty('--value', range.value * 0.1 + '%');
+  slider.style.setProperty('--value', `${range.value * 0.1}%`);
 });
