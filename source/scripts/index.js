8138ea9344ef8c326d6dbede22f35efba5074b2a
uzinok <41639106+uzinok@users.noreply.github.com>
2026-09-17 15:17:07 +0300

    делает навигацию без JS

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,7 +1,9 @@
 /* в этот файл добавляет скрипты*/
-const slider = document.querySelector('.before-after');
-const range = document.querySelector('.before-after__range-js');
+if (document.querySelector('.before-after') && document.querySelector('.before-after__range-js')) {
+  const slider = document.querySelector('.before-after');
+  const range = document.querySelector('.before-after__range-js');
 
-range.addEventListener('input', () => {
-  slider.style.setProperty('--value', `${range.value * 0.1}%`);
-});
+  range.addEventListener('input', () => {
+    slider.style.setProperty('--value', `${range.value * 0.1}%`);
+  });
+}
