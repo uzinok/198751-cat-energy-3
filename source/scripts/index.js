0af40ba836cc85fb7c5c3bdfa9f22fa4e41ff813
uzinok <41639106+uzinok@users.noreply.github.com>
2026-09-17 18:43:44 +0300

    добавляет карту (нужно доработать)

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -7,3 +7,48 @@ if (document.querySelector('.before-after') && document.querySelector('.before-a
     slider.style.setProperty('--value', `${range.value * 0.1}%`);
   });
 }
+
+if (document.querySelector('.header.no-js')) {
+  document.querySelector('.header.no-js').classList.remove('no-js');
+
+  const toggle = document.querySelector('.nav-toggle');
+  const nav = document.querySelector('.nav');
+
+  toggle.addEventListener('click', () => {
+    toggle.classList.toggle('nav-toggle--open');
+    nav.classList.toggle('nav--open');
+  });
+}
+
+const script = document.createElement('script');
+script.src = 'https://api-maps.yandex.ru/2.1/?lang=ru_RU'; // если есть ключ — добавь, если нет — можно без него
+script.type = 'text/javascript';
+document.head.appendChild(script);
+
+script.addEventListener('load', () => {
+  ymaps.ready(() => {
+    const myMap = new ymaps.Map('map', {
+      center: [59.938631, 30.323037],
+      zoom: 14,
+      controls: [] // убираем все элементы управления
+    }, {
+      suppressMapOpenBlock: true // именно здесь, во втором аргументе
+    });
+
+    myMap.behaviors.disable(['scrollZoom', 'drag', 'dblClickZoom']);
+
+    const myPlacemark = new ymaps.Placemark(
+      [59.938631, 30.323037],
+      { hintContent: 'Мы тут' },
+      {
+        iconLayout: 'default#image',
+        iconImageHref: '../images/map/map-pin.png',
+        iconImageSize: [57, 53],
+        iconImageOffset: [-28, -53],
+        suppressMapActions: true
+      }
+    );
+
+    myMap.geoObjects.add(myPlacemark);
+  });
+});
